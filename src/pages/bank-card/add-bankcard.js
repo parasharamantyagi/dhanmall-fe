363bcb3ac25eb365472a35aae2a58fb08e822563
aman_tevvo <aman.tyagi@tevvo.com>
2023-09-16 16:01:27 +0530

    bank card

--- src/pages/bank-card/add-bankcard.js
+++ src/pages/bank-card/add-bankcard.js
@@ -2,7 +2,8 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Footer from "../footer";
 import BankCardHeader from "../header/bank-card";
-import BankCardList from "./list";
+import TextField from "@mui/material/TextField";
+import { Button, Card, CardContent, Grid } from "@mui/material";
 
 export default function Addbankcard() {
   return (
@@ -14,7 +15,130 @@ export default function Addbankcard() {
       }}
     >
       <BankCardHeader title="Add Bank Card" />
-      <BankCardList />
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Grid item xs={12} sm container>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Actual Name"
+              name="full_name"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="IFSC Code"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Bank name"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Bank account"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="State"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="City"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Mobile number"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Account phone number"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Code"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Grid>
+        </CardContent>
+      </Card>
       <Footer />
     </Box>
   );
