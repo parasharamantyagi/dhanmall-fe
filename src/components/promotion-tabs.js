79609219afd84d5f7b20954e966f3a6f79353292
Bharat Chhabra <Bharat.chhabra@tevvo.com>
2023-09-18 11:46:44 +0530

    add promotion code components

--- src/components/promotion-tabs.js
+++ src/components/promotion-tabs.js
@@ -1,13 +1,15 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import { useTheme } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import { styled } from "@mui/material/styles";
-import PromotionLavel1 from "../pages/promotion/lavel_1";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import PromotionLavel1 from '../pages/promotion/lavel_1';
+import { defaultCurrencyFormat } from '../utils/common-utils';
+import { Button, TextField } from '@mui/material';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -41,52 +43,108 @@ const StyledTabs = styled((props) => (
     TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
   />
 ))({
-  "& .MuiTabs-indicator": {
-    display: "flex",
-    justifyContent: "center",
-    backgroundColor: "transparent",
+  '& .MuiTabs-indicator': {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
   },
-  "& .MuiTabs-indicatorSpan": {
+  '& .MuiTabs-indicatorSpan': {
     maxWidth: 40,
-    width: "100%",
-    backgroundColor: "#fff",
+    width: '100%',
+    backgroundColor: '#fff',
   },
 });
 
 const StyledTab = styled((props) => <Tab {...props} />)(({ theme }) => ({
-  textTransform: "none",
+  textTransform: 'none',
   fontWeight: theme.typography.fontWeightRegular,
   fontSize: theme.typography.pxToRem(16),
-  color: "rgba(255, 255, 255)",
-  "&.Mui-selected": {
-    color: "#fff",
+  color: 'rgba(255, 255, 255)',
+  '&.Mui-selected': {
+    color: '#fff',
   },
-  "&.Mui-focusVisible": {
-    backgroundColor: "rgba(100, 95, 228, 0.32)",
+  '&.Mui-focusVisible': {
+    backgroundColor: 'rgba(100, 95, 228, 0.32)',
   },
 }));
 
 function a11yProps(index) {
   return {
     id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
   };
 }
 
 export default function FullWidthTabs() {
   const theme = useTheme();
+
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const copyToClipboard = () => {
+    const copyText = 'https://luckydhanmall.com/#/register?r_code=EFFEECFC';
+    navigator.clipboard.writeText(copyText).then(() => {
+      alert('copied to clipboard');
+    });
+  };
+
   return (
-    <Box sx={{ bgcolor: "background.paper", width: "auto" }}>
+    <Box sx={{ bgcolor: 'background.paper', width: 'auto' }}>
       <Typography variant="h2" align="center" margin={3} gutterBottom>
         Bonus: ₹ 34.2
       </Typography>
-      <AppBar sx={{ background: "#000" }} position="static">
+      <Box py={3} display="flex" justifyContent="space-evenly">
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Typography gutterBottom variant="h3">
+            Total People
+          </Typography>
+          <Typography variant="p4">1</Typography>
+        </Box>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Typography gutterBottom variant="h3">
+            Contribution
+          </Typography>
+          <Typography variant="p4">{defaultCurrencyFormat(30)}</Typography>
+        </Box>
+      </Box>
+      <Box py={3}>
+        <Box display="flex" flexDirection="column">
+          <TextField
+            placeholder="My Promotion Code"
+            label="My Promotion Code"
+            variant="outlined"
+            value="EFFEECFC"
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+          <TextField
+            placeholder="My Promotion Link"
+            label="My Promotion Link"
+            variant="outlined"
+            value="https://luckydhanmall.com/#/register?r_code=EFFEECFC"
+            sx={{ mt: 2 }}
+            multiline
+            rows={4}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+        </Box>
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Button
+          onClick={copyToClipboard}
+          variant="contained"
+          sx={{ width: 400, mt: 2, mb: 4 }}
+        >
+          Copy Link
+        </Button>
+      </Box>
+      <AppBar sx={{ background: '#000' }} position="static">
         <StyledTabs
           value={value}
           onChange={handleChange}
