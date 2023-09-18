84c53a0641e27952a31e9b69f9f2885805ca1102
aman_tevvo <aman.tyagi@tevvo.com>
2023-09-19 00:46:47 +0530

    design

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,18 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Footer from "../footer";
 import Header from "../header";
+import { Stack } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  flexGrow: 1,
+}));
 
 export default function Home() {
   return (
@@ -13,7 +25,15 @@ export default function Home() {
       }}
     >
       <Header />
-      {/* <GamesTable /> */}
+      <Stack
+        spacing={{ xs: 1, sm: 2 }}
+        direction="row"
+        useFlexGap
+        flexWrap="wrap"
+      >
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+      </Stack>
       <Footer />
     </Box>
   );
