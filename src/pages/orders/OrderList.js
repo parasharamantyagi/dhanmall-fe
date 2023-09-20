477533adf57dd395ba2153ce3bc23f189eb49611
aman_tevvo <aman.tyagi@tevvo.com>
2023-09-20 23:46:53 +0530

    change style sheet

--- src/pages/orders/OrderList.js
+++ src/pages/orders/OrderList.js
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import useApi from "../../hooks/useApi";
 import { Skeleton } from "@mui/material";
-import { formatNewDateTime, getLastEightWords } from "../../utils/common-utils";
+import { formatNewDateTime, getLastEightWords, strictValidObjectWithKeys } from "../../utils/common-utils";
 
 export default function OrderList() {
   const { data, loading } = useApi("/order", "GET");
@@ -49,6 +49,36 @@ export default function OrderList() {
               <AccordionDetails>
                 <Typography>
                   <span className="bold-text">Price</span>: {order.price}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">contract_money</span>: {strictValidObjectWithKeys(order) ? order.contract_money : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">contract_type</span>: {strictValidObjectWithKeys(order) ? order.contract_type : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">contract_number</span>: {strictValidObjectWithKeys(order) ? order.contract_number : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">type</span>: {strictValidObjectWithKeys(order) ? order.type : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">pick</span>: {strictValidObjectWithKeys(order) ? order.pick : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">delivery</span>: {strictValidObjectWithKeys(order) ? order.delivery : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">fee</span>: {strictValidObjectWithKeys(order) ? order.fee : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">status</span>: {strictValidObjectWithKeys(order) ? order.status : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">goods_id</span>: {strictValidObjectWithKeys(order) ? order.goods_id : 'N/A'}
+                </Typography>
+                <Typography>
+                  <span className="bold-text">price</span>: {strictValidObjectWithKeys(order) ? order.price : 'N/A'}
                 </Typography>
               </AccordionDetails>
             </Accordion>
--- src/pages/win/index.js
+++ src/pages/win/index.js
@@ -3,16 +3,17 @@ import Box from "@mui/material/Box";
 import Footer from "../footer";
 import GamesTable from "./games";
 import Header from "../header";
-import { Divider, Typography } from "@mui/material";
+import { Button, Divider, Typography } from "@mui/material";
 import CurrentGame from "./current-game";
 import { gameNowService } from "./actions";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
-import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import OrderList from "../orders/OrderList";
-
+import { useNavigate } from "react-router-dom";
 
 export default function Win() {
   const [gameNow, setGameNow] = React.useState({ period: 0, time: 180 });
+  const navigate = useNavigate();
 
   const callapi = async () => {
     let game_now = await gameNowService();
@@ -64,6 +65,24 @@ export default function Win() {
         </Box>
         <Divider />
         <OrderList />
+        <Box
+          py={1.5}
+          display="flex"
+          justifyContent="center"
+          flexDirection="column"
+          alignItems="center"
+          // paddingTop={6}
+          paddingBottom={10}
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+            onClick={() => navigate("/orders")}
+          >
+            My Orders
+          </Button>
+        </Box>
       </Box>
       <Footer />
     </>
