cdaaf423152bc8a3b7088ba9eaf7d0c6d6617f30
aman_tevvo <aman.tyagi@tevvo.com>
2023-10-06 15:34:44 +0530

    fix code

--- src/pages/billing/GameNow.js
+++ src/pages/billing/GameNow.js
@@ -1,15 +1,34 @@
 import * as React from "react";
 import CanvasJSReact from "@canvasjs/react-charts";
 import { useBillingGameNowList } from "../../hooks/useBillingApi";
-import { strictValidObjectWithKeys } from "../../utils/common-utils";
+import {
+  strictValidObjectWithKeys,
+  validValue,
+} from "../../utils/common-utils";
+import {
+  Button,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
+import { useRechargeDetail } from "./actions";
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 export default function GameNow() {
-  const { billingGame } = useBillingGameNowList(
-    "billing/current-game",
-    "GET"
-  );
+  let apiCall = useRechargeDetail;
+  const { billingGame } = useBillingGameNowList("billing/current-game", "GET");
+  const [gameValue, setGameValue] = React.useState(10);
   const [object, setObject] = React.useState({
+    game_id: {
+      _id: 0,
+      date: 0,
+      period: "000",
+      begintime: 0,
+      detail: { set_unit: 0, set_value: 0 },
+    },
     total_price: { total_amount: 0, total_delivery: 0, pick_count: 0 },
     pick_red: { total_amount: 0, total_delivery: 0, pick_count: 0 },
     pick_green: { total_amount: 0, total_delivery: 0, pick_count: 0 },
@@ -26,6 +45,16 @@ export default function GameNow() {
     pick_9: { total_amount: 0, total_delivery: 0, pick_count: 0 },
   });
 
+  const handleSubmit = async () => {
+    if (gameValue !== "no") {
+      apiCall("billing/set-game", "POST", {
+        game_id: object.game_id._id,
+        set_unit: 1,
+        set_value: gameValue,
+      });
+    }
+  };
+
   React.useEffect(() => {
     if (strictValidObjectWithKeys(billingGame) && billingGame.success) {
       setObject(billingGame.billingGame);
@@ -34,11 +63,11 @@ export default function GameNow() {
 
   const options = {
     title: {
-      text: "Game prediction Chart",
+      text:
+        "Game prediction Chart " + object.game_id.date + object.game_id.period,
     },
     data: [
       {
-        // Change type to "doughnut", "line", "splineArea", etc.
         type: "column",
         dataPoints: [
           {
@@ -66,7 +95,7 @@ export default function GameNow() {
     exportEnabled: true,
     theme: "dark2", // "light1", "dark1", "dark2"
     title: {
-      text: "Trip Expenses",
+      text: "Color Expenses",
     },
     data: [
       {
@@ -74,23 +103,142 @@ export default function GameNow() {
         indexLabel: "{label}: {y}%",
         startAngle: -90,
         dataPoints: [
-          { y: object.pick_red.total_amount, label: "Red", color: "red" },
-          { y: object.pick_green.total_amount, label: "Violet", color: "violet" },
-          { y: object.pick_violet.total_amount, label: "Green", color: "green" },
+          {
+            y:
+              (object.pick_red.total_amount /
+                (object.pick_red.total_amount +
+                  object.pick_green.total_amount +
+                  object.pick_violet.total_amount)) *
+              100,
+            label: "Red(" + object.pick_red.total_amount + ")",
+            color: "red",
+          },
+          {
+            y:
+              (object.pick_violet.total_amount /
+                (object.pick_red.total_amount +
+                  object.pick_green.total_amount +
+                  object.pick_violet.total_amount)) *
+              100,
+            label: "Violet(" + object.pick_violet.total_amount + ")",
+            color: "violet",
+          },
+          {
+            y:
+              (object.pick_green.total_amount /
+                (object.pick_red.total_amount +
+                  object.pick_green.total_amount +
+                  object.pick_violet.total_amount)) *
+              100,
+            label: "Green(" + object.pick_green.total_amount + ")",
+            color: "green",
+          },
         ],
       },
     ],
   };
   return (
     <React.Fragment>
-      <CanvasJSChart
-        options={options}
-        /* onRef={ref => this.chart = ref} */
-      />
-      <CanvasJSChart
-        options={options1}
-        /* onRef={ref => this.chart = ref} */
-      />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={9}>
+          <CanvasJSChart
+            options={options}
+            /* onRef={ref => this.chart = ref} */
+          />
+          <Divider />
+          <Divider />
+          <Divider />
+          <Divider />
+          <Divider />
+          <Divider />
+          <CanvasJSChart
+            options={options1}
+            /* onRef={ref => this.chart = ref} */
+          />
+        </Grid>
+        <Grid item xs={12} sm={3} mt={4}>
+          {"Set Value -> "}
+          {strictValidObjectWithKeys(object) &&
+          strictValidObjectWithKeys(object.game_id.detail) &&
+          validValue(object.game_id.detail.set_unit)
+            ? object.game_id.detail.set_value
+            : "No"}
+          <br />
+          <FormControl>
+            <RadioGroup name="set_number" defaultValue={10}>
+              <FormControlLabel
+                control={<Radio />}
+                value={0}
+                label="0"
+                onClick={() => setGameValue(0)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={1}
+                label="1"
+                onClick={() => setGameValue(1)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={2}
+                label="2"
+                onClick={() => setGameValue(2)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={3}
+                label="3"
+                onClick={() => setGameValue(3)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={4}
+                label="4"
+                onClick={() => setGameValue(4)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={5}
+                label="5"
+                onClick={() => setGameValue(5)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={6}
+                label="6"
+                onClick={() => setGameValue(6)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={7}
+                label="7"
+                onClick={() => setGameValue(7)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={8}
+                label="8"
+                onClick={() => setGameValue(8)}
+              />
+              <FormControlLabel
+                control={<Radio />}
+                value={9}
+                label="9"
+                onClick={() => setGameValue(9)}
+              />
+            </RadioGroup>
+            <Button
+              type="submit"
+              size="medium"
+              variant="contained"
+              sx={{ mt: 3, mb: 7 }}
+              onClick={handleSubmit}
+            >
+              Set
+            </Button>
+          </FormControl>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 }
--- src/pages/billing/index.js
+++ src/pages/billing/index.js
@@ -1,22 +1,46 @@
 import * as React from "react";
 import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import MuiDrawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import Container from "@mui/material/Container";
+import Badge from "@mui/material/Badge";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 import MainList from "./listItems";
 import BillingOrders from "./BillingOrders";
 import GameNow from "./GameNow";
 import RechargeReq from "./RechargeReq";
 import WithdrawalReq from "./WithdrawalReq";
+import { Card } from "@mui/material";
 
 const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
@@ -49,7 +73,6 @@ const defaultTheme = createTheme();
 
 export default function BillingDashboard({ routeName }) {
   const [open, setOpen] = React.useState(true);
-
   const toggleDrawer = () => {
     setOpen(!open);
   };
@@ -57,6 +80,41 @@ export default function BillingDashboard({ routeName }) {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: "24px", // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                ...(open && { display: "none" }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Dashboard
+            </Typography>
+            <IconButton color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
         <Drawer variant="permanent" open={open}>
           <Toolbar
             sx={{
@@ -76,29 +134,26 @@ export default function BillingDashboard({ routeName }) {
             <Divider sx={{ my: 1 }} />
           </List>
         </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-          }}
-        >
-          <Toolbar />
-          <Container fixed sx={{ mt: 4, mb: 4 }}>
-            <Grid>
-              <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                {routeName === "luck67" && <BillingOrders />}
-                {routeName === "game-now" && <GameNow />}
-                {routeName === "recharge-req" && <RechargeReq />}
-                {routeName === "withdrawal-req" && <WithdrawalReq />}
-              </Paper>
-            </Grid>
-          </Container>
+        <Box p={1} flexDirection="column" display="flex">
+          <Box
+            noValidate
+            sx={{
+              display: "flex",
+              minHeight: "100vh",
+              flexDirection: "column",
+            }}
+          >
+            <Card sx={{ width: 1600, mt: 6, mb: 4 }}>
+              <Grid>
+                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+                  {routeName === "luck67" && <BillingOrders />}
+                  {routeName === "game-now" && <GameNow />}
+                  {routeName === "recharge-req" && <RechargeReq />}
+                  {routeName === "withdrawal-req" && <WithdrawalReq />}
+                </Paper>
+              </Grid>
+            </Card>
+          </Box>
         </Box>
       </Box>
     </ThemeProvider>
