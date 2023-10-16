db1aedc21815703dcbbb62d185ab4e4f749760f4
aman_tevvo <aman.tyagi@tevvo.com>
2023-10-16 11:10:17 +0530

    okk

--- src/pages/wallet/walletRecharge.js
+++ src/pages/wallet/walletRecharge.js
@@ -9,31 +9,31 @@ import {
   Grid,
   TextField,
   Typography,
-} from '@mui/material';
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import CardHeader from '../header/header-card';
-import Paper from '@mui/material/Paper';
-import Footer from '../footer';
+} from "@mui/material";
+import React from "react";
+import { styled } from "@mui/material/styles";
+import CardHeader from "../header/header-card";
+import Paper from "@mui/material/Paper";
+import Footer from "../footer";
 import {
   validValue,
   defaultCurrencyFormat,
   validObjectWithParameterKeys,
   strictValidObjectWithKeys,
-} from '../../utils/common-utils';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+} from "../../utils/common-utils";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 // import { appUpiId } from "../../utils/constant";
-import { addRechargeService } from './action';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { rechargeAmmount } from '../../utils/constant';
+import { addRechargeService } from "./action";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { rechargeAmmount } from "../../utils/constant";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 export default function WalletRecharge() {
@@ -41,7 +41,7 @@ export default function WalletRecharge() {
   const profile = useSelector((state) => state.profile.data);
   const [objVal, setObjVal] = React.useState({});
   const [is_show, setIsShow] = React.useState(0);
-  const [type, setType] = React.useState('');
+  const [type, setType] = React.useState("");
 
   const renderSubtitle = (text) => {
     return (
@@ -55,61 +55,61 @@ export default function WalletRecharge() {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     let newObj = {
-      ammount: data.get('recharge_amount'),
-      transaction_id: data.get('transaction_id'),
-      remarks: data.get('remarks'),
+      ammount: data.get("recharge_amount"),
+      transaction_id: data.get("transaction_id"),
+      remarks: data.get("remarks"),
       type: type,
     };
     let result = await addRechargeService(newObj);
     if (result.success) {
       toast.success(result.message);
-      navigate('/wallet-transactions');
+      navigate("/wallet-transactions");
     } else {
       toast.error(result.message);
     }
   };
 
   const handleChange = (e) => {
+    console.log(objVal,profile);
     if (
       strictValidObjectWithKeys(objVal) &&
       validValue(objVal.recharge_amount)
     ) {
       if (
-        !profile.first_payment &&
+        profile.first_payment === 0 &&
         parseInt(objVal.recharge_amount) < rechargeAmmount[0].ammount
       ) {
         toast.error(
-          `First payment should be minimum ${rechargeAmmount[0].ammount} rupees`,
+          `First payment should be minimum ${rechargeAmmount[0].ammount} rupees`
         );
         return false;
       }
       setIsShow(
-        e.target.name === 'qrcode' ? 1 : e.target.name === 'upipay' ? 2 : 0,
+        e.target.name === "qrcode" ? 1 : e.target.name === "upipay" ? 2 : 0
       );
       setType(e.target.name);
     } else {
-      toast.error('Please select an amount first.');
+      toast.error("Please select an amount first.");
     }
   };
 
-
   const copyToClipboard = () => {
-    navigator.clipboard.writeText('parasharamantyagi-2@okhdfcbank').then(() => {
-      toast.success('Copied to Clipboard');
+    navigator.clipboard.writeText("parasharamantyagi-2@okhdfcbank").then(() => {
+      toast.success("Copied to Clipboard");
     });
   };
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        minHeight: '100vh',
-        flexDirection: 'column',
+        display: "flex",
+        minHeight: "100vh",
+        flexDirection: "column",
       }}
     >
       <CardHeader title="Recharge" />
       <Box p={1} flexDirection="column" display="flex">
-        <Box component="form" onSubmit={handleSubmit} sx={{ width: '100%' }}>
+        <Box component="form" onSubmit={handleSubmit} sx={{ width: "100%" }}>
           <Grid
             container
             rowSpacing={1}
@@ -131,9 +131,9 @@ export default function WalletRecharge() {
                     name="recharge_amount"
                     autoComplete="recharge_amount"
                     value={
-                      validObjectWithParameterKeys(objVal, ['recharge_amount'])
+                      validObjectWithParameterKeys(objVal, ["recharge_amount"])
                         ? objVal.recharge_amount
-                        : ''
+                        : ""
                     }
                     onChange={(event) =>
                       setObjVal({
@@ -158,11 +158,11 @@ export default function WalletRecharge() {
                       <Button
                         variant="contained"
                         sx={{
-                          width: '80%',
+                          width: "80%",
                           py: 1,
                           mt: 1,
                           mb: 1,
-                          background: '#000000',
+                          background: "#000000",
                         }}
                         onClick={() => {
                           setObjVal({ recharge_amount: object.ammount });
@@ -195,7 +195,7 @@ export default function WalletRecharge() {
                         <FormControlLabel
                           control={
                             <Checkbox
-                              checked={type === 'qrcode'}
+                              checked={type === "qrcode"}
                               onChange={handleChange}
                               name="qrcode"
                             />
@@ -205,7 +205,7 @@ export default function WalletRecharge() {
                         <FormControlLabel
                           control={
                             <Checkbox
-                              checked={type === 'upipay'}
+                              checked={type === "upipay"}
                               onChange={handleChange}
                               name="upipay"
                             />
@@ -228,7 +228,7 @@ export default function WalletRecharge() {
                           mt={1}
                           variant="h2"
                           align="left"
-                          marginBottom={'5px'}
+                          marginBottom={"5px"}
                         >
                           By using this QR code or UPI ID, you can recharge the
                           amount and submit the page with the correct
@@ -240,9 +240,9 @@ export default function WalletRecharge() {
                             width: 300,
                             height: 300,
                             margin: 1,
-                            align: 'center',
+                            align: "center",
                           }}
-                          image="/img/11.png"
+                          image="/img/11.jpeg"
                         />
                         <Typography
                           variant="body2"
@@ -258,24 +258,24 @@ export default function WalletRecharge() {
                           placeholder="Transaction ID"
                           label="Transaction ID"
                           variant="outlined"
-                          sx={{ mt: 2, width: '100%' }}
+                          sx={{ mt: 2, width: "100%" }}
                         />
                         <TextField
                           name="remarks"
                           placeholder="Enter Remarks if you mention in your upi"
                           label="Remarks"
                           variant="outlined"
-                          sx={{ mt: 2, width: '100%' }}
+                          sx={{ mt: 2, width: "100%" }}
                         />
                         <Button
                           type="submit"
                           variant="contained"
                           sx={{
-                            width: '100%',
+                            width: "100%",
                             py: 1,
                             mt: 1,
                             mb: 4,
-                            background: '#000000',
+                            background: "#000000",
                           }}
                         >
                           Recharge
@@ -302,12 +302,12 @@ export default function WalletRecharge() {
                             placeholder="UPI ID"
                             label="UPI ID"
                             variant="outlined"
-                            value={'parasharamantyagi-2@okhdfcbank'}
+                            value={"parasharamantyagi-2@okhdfcbank"}
                             InputProps={{
                               readOnly: true,
                             }}
                             sx={{
-                              width: '40%',
+                              width: "40%",
                               mt: 2,
                               mb: 2,
                             }}
@@ -317,7 +317,7 @@ export default function WalletRecharge() {
                             variant="contained"
                             sx={{
                               ml: 4,
-                              background: '#000000',
+                              background: "#000000",
                             }}
                           >
                             Copy UPI ID
@@ -353,23 +353,23 @@ export default function WalletRecharge() {
                           placeholder="Transaction ID"
                           label="Transaction ID"
                           variant="outlined"
-                          sx={{ mt: 2, width: '100%' }}
+                          sx={{ mt: 2, width: "100%" }}
                         />
                         <TextField
                           name="remarks"
                           placeholder="Enter Remarks if you mention in your upi"
                           label="Remarks"
                           variant="outlined"
-                          sx={{ mt: 2, width: '100%' }}
+                          sx={{ mt: 2, width: "100%" }}
                         />
                         <Button
                           type="submit"
                           variant="contained"
                           sx={{
-                            width: '100%',
+                            width: "100%",
                             py: 1,
                             mt: 4,
-                            background: '#000000',
+                            background: "#000000",
                           }}
                         >
                           Recharge
